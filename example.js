c72de9f8f5ade06a2fd31764a1e185dc0c055782
Alfred Westerveld <alfredwesterveld@gmail.com>
2016-01-13 12:54:58 +0100

    update example.js for cleaner tonic output

--- example.js
+++ example.js
@@ -1,38 +1,38 @@
-var assert = require('assert')
 var Q = require('queuetry')
 var q = Q()
 var i = 0
 var res = []
 
-// put first synchronously.
+// put first synchronously (before get).
 q.put("a")
 
-// get what was allready put("a") above.
+// get what was allready put (above).
 q.getAsync(function(item) {
-    res.push([i++, item]) // => [0, "a"]
-    return true // really deleted from queue
+    var j = [i++, item]
+    console.log(j) // [0, 'a']
+    res.push(j)
+    return true // deleted from queue.
 })
 
-// async get put("b") below.
+// async get because no put
 q.getAsync(function(item) {
-    res.push([i++, item]) // => [1, "b"]
-    return false // not deleted from queue, next getAsync() will get this message
+    var j = [i++, item]
+    console.log(j) // [1, 'b']
+    res.push(j)
+    return false // peeked from queue, next successfull getAsync() will delete.
 })
 
-// async get put("b") below because previous  getAsync failed
+// async get because no put
 q.getAsync(function(item) {
-    res.push([i++, item]) // => [2, "b"]
+    var j = [i++, item] // [2, 'b']
+    console.log(j)
+    res.push(j)
     return true
 })
 
 q.put("b") // put after get.
 
-// before exit we check
-process.on('exit', function () {
-    console.log(res)
-    assert.deepEqual(res, [
-        [0, "a"],
-        [1, "b"],
-        [2, "b"]
-    ])
-})
+// Expected output =>
+// [ 0, 'a' ]
+// [ 1, 'b' ]
+// [ 2, 'b' ]
